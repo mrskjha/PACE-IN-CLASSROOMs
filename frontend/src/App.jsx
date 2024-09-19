bb9cc2ecf7c462c1b9b0a07c77772d072de2999e
priya <priyaaashu4@gmail.com>
2024-09-19 21:45:39 +0530

    added footer flie

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@
 import Navbar from './components/Navbar'
 import Header from './components/Header';
 import NewEvents from './components/NewEvents';
+import Footer from './components/Footer';
 
 export const App = () => {
   return (
     <div><Navbar />
     <Header />
     <NewEvents/>
+    <Footer />
     </div>
   )
 }
