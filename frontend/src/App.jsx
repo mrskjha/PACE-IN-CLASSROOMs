ca546a7e6d5c1c6b9b8fe29960fd8a46079232f5
priya <priyaaashu4@gmail.com>
2024-09-19 15:45:22 +0530

    added new events file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 
 import Navbar from './components/Navbar'
 import Header from './components/Header';
+import NewEvents from './components/NewEvents';
 
 export const App = () => {
   return (
     <div><Navbar />
     <Header />
+    <NewEvents/>
     </div>
   )
 }
