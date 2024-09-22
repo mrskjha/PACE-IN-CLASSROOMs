a5a821be10498b75342511eb703e1847aad8d0fe
priya <priyaaashu4@gmail.com>
2024-09-22 22:27:47 +0530

    added login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Header from './components/Header';
 import NewEvents from './components/NewEvents';
 import Footer from './components/Footer';
+import Login from './components/login';
 
 export const App = () => {
   return (
-    <div><Navbar />
+    <div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Login />} /> {/* You can use the same component */}
+        {/* Other routes */}
+      </Routes>
+    </Router>
     <Header />
     <NewEvents/>
     <Footer />
+    
     </div>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 
-
+import { Link } from "react-router-dom";
+import Login from "./login";
 const Navbar = () => {
   return (
     <nav>
@@ -19,12 +20,17 @@ const Navbar = () => {
             
         </div>
         <div className="relative flex justify-center items-center gap-6 p-4">
+  <Link to='/signup'> 
   <button className=" text-indigo-600 font-semibold py-1 px-10 rounded-[5%] hover:bg-blue-600 hover:text-white transition duration-300 ease-in-out shadow-lg">
     SignUp
   </button>
+  </Link>    
+  <Link to="/login">
   <button className="bg-transparent bg-blue-500 text-blue-500 font-semibold py-1 px-10 rounded-[5%] border-2 border-blue-500 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out shadow-lg">
     Login
   </button>
+  </Link>
+
 </div>
 
 
