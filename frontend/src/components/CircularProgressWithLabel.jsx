0c0445b3d52848ee015dfa9343907b0d9c7dec86
Sunny <jhasunny@gmail.com>
2024-10-01 03:59:46 +0530

    Add dependencies for Emotion, MUI, and CircularProgressWithLabel component

---
+++ frontend/src/components/CircularProgressWithLabel.jsx
@@ -0,0 +1,40 @@
+// CircularProgressWithLabel.js
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function CircularProgressWithLabel(props) {
+  return (
+    <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+      <CircularProgress variant="determinate" {...props} />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography
+          variant="caption"
+          component="div"
+          sx={{ color: 'white' }}
+        >
+          {`${Math.round(props.value)}%`}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
+CircularProgressWithLabel.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+export default CircularProgressWithLabel;
